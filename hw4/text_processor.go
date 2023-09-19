93d1a77a651e8a78e1facb23f2d3aa05c2e6014a
kyrylyuk andriy <kyrylyuk.a@gmail.com>
2023-09-19 13:55:11 -0400

    add search functionality

--- hw4/text_processor.go
+++ hw4/text_processor.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strings"
 )
 
 func ReadLines(input io.Reader) []string {
@@ -30,4 +31,17 @@ func PrintLines(text []string, output io.Writer) {
 	for _, v := range text {
 		fmt.Fprintln(output, v)
 	}
+}
+
+func SearchLines(text []string, keyword string) {
+	var result []string
+	for _, v := range text {
+		if strings.Contains(v, keyword) {
+			result = append(result, v)
+		}
+	}
+	if len(result) == 0 {
+		fmt.Println("no strings found based on your search query")
+	}
+	fmt.Println(result)
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kyrylyuk-andriy/golang-homework/hw4"
@@ -8,5 +9,10 @@ import (
 
 func main() {
 	lines := hw4.ReadLines(os.Stdin)
+	// optionally to print all strings to compare search result
 	hw4.PrintLines(lines, os.Stdout)
+	var searchQuery string
+	fmt.Print("What is your search query?")
+	fmt.Scan(&searchQuery)
+	hw4.SearchLines(lines, searchQuery)
 }
