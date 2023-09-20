89a7a00f0b36ac5526801fb4f8aa300c0d3e6bde
kyrylyuk andriy <kyrylyuk.a@gmail.com>
2023-09-19 20:30:23 -0400

    add average student score function

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	// "fmt"
+	// "os"
 
 	"github.com/kyrylyuk-andriy/golang-homework/hw4"
 )
 
 func main() {
-	lines := hw4.ReadLines(os.Stdin)
+	//
+	// HW4 task1
+	//
+	// lines := hw4.ReadLines(os.Stdin)
 	// optionally to print all strings to compare search result
-	hw4.PrintLines(lines, os.Stdout)
-	var searchQuery string
-	fmt.Print("What is your search query?")
-	fmt.Scan(&searchQuery)
-	hw4.SearchLines(lines, searchQuery)
+	// hw4.PrintLines(lines, os.Stdout)
+	// var searchQuery string
+	// fmt.Print("What is your search query?")
+	// fmt.Scan(&searchQuery)
+	// hw4.SearchLines(lines, searchQuery)
+
+	//
+	//HW4 task2
+	hw4.Average_score()
 }
