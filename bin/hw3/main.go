367d3e452424ed4b74e680447b79e0de74b9336f
kyrylyuk andriy <kyrylyuk.a@gmail.com>
2023-09-27 06:21:49 -0400

    move hw3 to bin

---
+++ bin/hw3/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kyrylyuk-andriy/golang-homework/hw3"
+)
+
+func main() {
+	hw3.Game()
+}
--- hw3/game.go
+++ hw3/game.go
@@ -1,18 +1,18 @@
-package homework
+package hw3
 
-import ( 
-	"fmt"
-	"os"
-	"math/rand"
+import (
 	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
 	"strconv"
 	"strings"
 )
 
 type Character struct {
-	Name string
+	Name     string
 	Sleeping bool
-	Stuff []string 
+	Stuff    []string
 }
 
 // Check if main character is sleeping
@@ -35,12 +35,11 @@ var tentAnswer string
 var min, max, attempts int
 var secretNumber int
 
-
 func Game() {
-	character := Character {"Steven", true, []string{"backpack", "knife", "flashlight", "match"}}
+	character := Character{"Steven", true, []string{"backpack", "knife", "flashlight", "match"}}
 	if character.Sleeping == true {
 		fmt.Printf("%s is sleeping right now, Lets wake up him\n", character.Name)
-		goto WakeUp 
+		goto WakeUp
 	} else {
 		goto CaveEntry
 	}
@@ -49,7 +48,7 @@ WakeUp:
 	character.WakeUP()
 	if character.Sleeping == false {
 		fmt.Printf("%s is not sleeping\n", character.Name)
-		goto CaveEntry 
+		goto CaveEntry
 	}
 
 CaveEntry:
@@ -58,50 +57,50 @@ CaveEntry:
 	fmt.Print("Should i go inside to a cave or use path into a forest? yes -> go to a cave, no -> go to a forest\n")
 	fmt.Scan(&caveEntryAnswer)
 	switch caveEntryAnswer {
-		case "yes":
-			fmt.Print("You answered yes, lets go to a cave and check what is going on there\n")
-			goto Cave
-		case "no":
-			fmt.Print("You answered no, lets use that path and go to the Forest\n")
-			goto Forest
-		default:
-			fmt.Print("You didn't input right answer, you could use yes or no (lowercase)")
-			goto CaveEntry
+	case "yes":
+		fmt.Print("You answered yes, lets go to a cave and check what is going on there\n")
+		goto Cave
+	case "no":
+		fmt.Print("You answered no, lets use that path and go to the Forest\n")
+		goto Forest
+	default:
+		fmt.Print("You didn't input right answer, you could use yes or no (lowercase)")
+		goto CaveEntry
 	}
 
 Cave:
 	fmt.Print("I am in a cave, lets turn on our lighter. There is a ladder inside from the cave\n")
-	goto Exit 
+	goto Exit
 
 Forest:
 	fmt.Print("I am in a forest, I see dead body of the animal, What should i do? skip or check ?  \n")
 	fmt.Print("possible answer : skip and check \n")
 	fmt.Scan(&deadBodyAnimalAnswer)
 	switch deadBodyAnimalAnswer {
-		case "skip":
-			fmt.Print("Ok, your answer is skip and you will go ahead\n ")
-			goto Camp
-		case "check":
-			fmt.Print("Ok, your answer is check, animal was poisened and you are dead too\n ")
-			goto LoseGame
-		default:
-			fmt.Print("Your answer is not correct, you can use skip and check (lowercase)\n ")
-			goto Forest 
+	case "skip":
+		fmt.Print("Ok, your answer is skip and you will go ahead\n ")
+		goto Camp
+	case "check":
+		fmt.Print("Ok, your answer is check, animal was poisened and you are dead too\n ")
+		goto LoseGame
+	default:
+		fmt.Print("Your answer is not correct, you can use skip and check (lowercase)\n ")
+		goto Forest
 	}
 Camp:
 	fmt.Print("I am in a empty camp and i see a tent. I am tired and would like to have a rest \n ")
 	fmt.Print("Should i use a tent or should i go. Possible answer: go or rest ?\n")
 	fmt.Scan(&tentAnswer)
 	switch tentAnswer {
-		case "rest":
-			fmt.Print("Lets use that tent and have a rest\n")
-			goto Tent
-		case "go":
-			fmt.Print("It saved your life, there is a dangerous bug in a tent\n ")
-			goto Exit
-		default:
-			fmt.Print("Your answer is not correct, you can use rest or go (all lowercase), lets try again\n")
-			goto Camp
+	case "rest":
+		fmt.Print("Lets use that tent and have a rest\n")
+		goto Tent
+	case "go":
+		fmt.Print("It saved your life, there is a dangerous bug in a tent\n ")
+		goto Exit
+	default:
+		fmt.Print("Your answer is not correct, you can use rest or go (all lowercase), lets try again\n")
+		goto Camp
 	}
 
 Tent:
@@ -113,7 +112,7 @@ Tent:
 	max = 20
 	attempts = 0
 	secretNumber = rand.Intn(max-min) + min
-	
+
 	for {
 		attempts++
 		reader := bufio.NewReader(os.Stdin)
@@ -146,6 +145,6 @@ Tent:
 LoseGame:
 	fmt.Print("GAME OVER\n")
 	os.Exit(0)
-Exit: 
+Exit:
 	fmt.Print("you are saved, you found how to espace\n")
 }
